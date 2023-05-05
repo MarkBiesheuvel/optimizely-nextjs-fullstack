81cf2f3a2ed510cd070fa38485cb6ebdd692c4e5
Mark Biesheuvel <mail@markbiesheuvel.nl>
2023-05-05 13:35:30 +0200

    Avoid hydration errors (again)

--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,52 @@
+import { useState, } from 'react';
+import { Spinner } from 'react-bootstrap';
 import {
   OptimizelyProvider,
   useDecision
 } from '@optimizely/react-sdk';
 
-const Content = () => {
-  const [ decision ] = useDecision('sorting_algorithm');
+const Content = ({optimizely}) => {
+  // Get Optimizely decision
+  const [ decision, clientReady ] = useDecision('sorting_algorithm');
   const { variables, variationKey } = decision;
   const { direction, field } = variables;
 
-  return (
-    <>
-      <h1>Hello World!</h1>
-      <div>The sort order is {field} {direction}</div>
-      <div>{ variationKey }</div>
-    </>
-  );
+  // Set visible state equal to the clientReady boolean
+  const [visible, setVisible] = useState(clientReady);
+
+  // Update the visibility if the client becomes ready later
+  if (!clientReady) {
+    optimizely.onReady().then(() => {
+      setVisible(true);
+    })
+  }
+
+  if (visible) {
+    // Actual content
+    return (
+      <>
+        <div>The sort order is {field} {direction}</div>
+        <div>{ variationKey }</div>
+      </>
+    );
+  } else {
+    // Loading icon
+    return (
+      <Spinner animation='border' role='status'>
+        <span className='visually-hidden'>Loading...</span>
+      </Spinner>
+    );
+  }
 };
 
 const Page = ({optimizely, user}) => {
   return (
-    <OptimizelyProvider optimizely={optimizely} user={user}>
-      <Content />
-    </OptimizelyProvider>
+    <>
+      <h1>Hello {user.id}!</h1>
+      <OptimizelyProvider optimizely={optimizely} user={user}>
+        <Content optimizely={optimizely} />
+      </OptimizelyProvider>
+    </>
   )
 };
 
